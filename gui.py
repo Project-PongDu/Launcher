178db96fb448a03486157358b54bb221047a99ce
t3qquq <63445961+t3qquq@users.noreply.github.com>
2026-07-09 13:31:06 +0900

    [Fix] 화이트리스트 url 수정

--- gui.py
+++ gui.py
@@ -50,11 +50,18 @@ from PyQt5.QtWidgets import (
 #    3) JSON 객체       {"이름":"uuid", ...}  또는  {"whitelist":[...]}
 
 
+
+
+
 VERSION = "v3.2.1"
 
+WHITELIST_URL = "https://raw.githubusercontent.com/Project-PongDu/Whitelist/refs/heads/main/streamer%20whitelist.json"
 
 
-WHITELIST_URL = "https://raw.githubusercontent.com/t3qquq/myPZ-Configs/refs/heads/main/streamer%20whitelist.json"
+
+
+
+
 UA = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
 FORCE_ONLINE = False
 
